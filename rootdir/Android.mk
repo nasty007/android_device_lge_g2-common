59959d3a4e42fbacd563a03aad5dc6d6f25dd90b
Ícaro Hoff <icarohoff@gmail.com>
2016-12-31 15:32:38 +0100

    g2-common: ultimate device tree cleanup/optimization * Changed all headers to Paranoid Android. * Modified the init files and boot hardware to use qcom hardware. * Changed the prox. sensor delay. * Removed obsolete console cmdline, why this still here?
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>
    
    Picked from original commit
    https://github.com/GalaticStryder/device_lge_g2-common/commit/c86b32d290c63efe87a776fdafddb5b306a11eac#diff-8472ffe72e0c5f6647d51f32a743ec77

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,10 +3,11 @@ LOCAL_PATH := $(call my-dir)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := set_baseband.sh
+LOCAL_MODULE            := init.baseband.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/set_baseband.sh
+LOCAL_SRC_FILES         := etc/init.baseband.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 # Init scripts
